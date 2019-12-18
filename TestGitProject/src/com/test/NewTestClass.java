70e487256bfafe37f6d8643c460c630c3bc8c742
Meenakshi <meenakshi.kanaujia@gmail.com>
2019-12-18 12:58:41 +0530

    added the comment

--- TestGitProject/src/com/test/NewTestClass.java
+++ TestGitProject/src/com/test/NewTestClass.java
@@ -5,6 +5,7 @@ public class NewTestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("new Test Class");
+		System.out.println("new comment added");
 	}
 
 }
